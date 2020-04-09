72e557f9cdb0ed0c2d578c1b7401e8732196aae8
Charlie Duong <charlieduong94@gmail.com>
2020-04-09 15:37:38 -0400

    Remove integration action until needed

--- src/framework/execution/types/action.ts
+++
@@ -1,9 +0,0 @@
-export type IntegrationAction = IntegrationIngestAction;
-
-export enum IntegrationActionName {
-  INGEST = 'INGEST',
-}
-
-export interface IntegrationIngestAction {
-  name: IntegrationActionName.INGEST;
-}
--- src/framework/execution/types/index.ts
+++ src/framework/execution/types/index.ts
@@ -2,5 +2,4 @@ export * from './context';
 export * from './step';
 export * from './config';
 export * from './validation';
-export * from './action';
 export * from './instance';
