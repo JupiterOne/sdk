6025878d48b12621565393a4a5e54e9ed0ad66b3
Charlie Duong <charlieduong94@gmail.com>
2020-04-09 20:41:29 -0400

    Fix type issue

--- src/framework/execution/__tests__/executeIntegration.test.ts
+++ src/framework/execution/__tests__/executeIntegration.test.ts
@@ -2,8 +2,6 @@ import { IntegrationExecutionContext } from '../types';
 import { executeIntegrationLocally } from '../executeIntegration';
 import { LOCAL_INTEGRATION_INSTANCE } from '../instance';
 
-import Logger from 'bunyan';
-
 test('should execute validator function if provided in config', async () => {
   const validate = jest.fn();
 
