ff6c9f6d3b0b416d177c37f735b9e8fc37592d56
Austin Kelleher <austin.kelleher@lifeomic.com>
2020-05-11 11:01:07 -0400

    Allow specifying local JupiterOne account id in JUPITERONE_LOCAL_INTEGRATION_INSTANCE_ACCOUNT_ID environment variable

--- src/framework/execution/__tests__/instance.test.ts
+++ src/framework/execution/__tests__/instance.test.ts
@@ -2,10 +2,12 @@ import {
   createIntegrationInstanceForLocalExecution,
   LOCAL_INTEGRATION_INSTANCE,
 } from '../instance';
+import { v4 as uuid } from 'uuid';
 
 describe('createIntegrationInstanceForLocalExecution', () => {
   beforeEach(() => {
     process.env.MY_FIELD = 'test';
+    delete process.env.JUPITERONE_LOCAL_INTEGRATION_INSTANCE_ACCOUNT_ID;
   });
 
   afterEach(() => {
@@ -19,6 +21,20 @@ describe('createIntegrationInstanceForLocalExecution', () => {
     });
 
     expect(instance).toEqual(LOCAL_INTEGRATION_INSTANCE);
+  });
+
+  test('should allow specifying local JupiterOne account id through environment variable', async () => {
+    const accountId = (process.env.JUPITERONE_LOCAL_INTEGRATION_INSTANCE_ACCOUNT_ID = uuid());
+
+    const instance = await createIntegrationInstanceForLocalExecution({
+      validateInvocation: jest.fn(),
+      integrationSteps: [],
+    });
+
+    expect(instance).toEqual({
+      ...LOCAL_INTEGRATION_INSTANCE,
+      accountId,
+    });
   });
 
   test('should load config from env onto instance', async () => {
