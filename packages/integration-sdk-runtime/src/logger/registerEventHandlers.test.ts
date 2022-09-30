c174cf4fccf7077df020aefa907509cca1d8120e
Matthew Zember <matthew.zember@jupiterone.com>
2022-09-30 15:44:48 -0400

    add test that event handlers are registered

---
+++ packages/integration-sdk-runtime/src/logger/registerEventHandlers.test.ts
@@ -0,0 +1,27 @@
+import {
+  registerIntegrationLoggerEventHandlers,
+  registerEventHandlers,
+} from './registerEventHandlers';
+
+test('registerEventHandlers registers a handler for unhandledRejection, uncaughtException, and multipleResolves', () => {
+  const noopCallback = (err, event) => {
+    return;
+  };
+
+  const multipleResolvesCount = process.listeners('multipleResolves').length;
+  const uncaughtExceptionCount = process.listeners('uncaughtException').length;
+  const unhandledRejectionCount =
+    process.listeners('unhandledRejection').length;
+
+  registerEventHandlers(noopCallback);
+
+  expect(process.listeners('multipleResolves').length).toBe(
+    multipleResolvesCount + 1,
+  );
+  expect(process.listeners('uncaughtException').length).toBe(
+    uncaughtExceptionCount + 1,
+  );
+  expect(process.listeners('unhandledRejection').length).toBe(
+    unhandledRejectionCount + 1,
+  );
+});
