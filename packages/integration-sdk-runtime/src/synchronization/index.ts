e258fbcce2cfa17eb7d3ba55259df55dde3e0408
Matthew Zember <matthew.zember@jupiterone.com>
2022-10-16 17:09:58 -0400

    add debug log to upload data

--- packages/integration-sdk-runtime/src/synchronization/index.ts
+++ packages/integration-sdk-runtime/src/synchronization/index.ts
@@ -486,6 +486,16 @@ export async function uploadDataChunk<
           },
         },
       );
+
+      logger.debug(
+        {
+          uploadCorrelationId,
+          uploadType: type,
+          attemptNum: ctx.attemptNum,
+          batchSize: batch.length,
+        },
+        'Finished uploading batch',
+      );
     },
     {
       maxAttempts: 5,
