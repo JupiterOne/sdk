b3fd2dd6931f09c8bff3cad05efd677d186b5f05
Austin Kelleher <austinlkelleher@gmail.com>
2022-11-27 19:00:36 -0500

    Update Jest test snapshots

--- packages/cli/src/__tests__/cli-export.test.ts
+++ packages/cli/src/__tests__/cli-export.test.ts
@@ -64,12 +64,12 @@ test('should export assets', async () => {
     ]),
   ).resolves.toEqual(expect.anything());
   expect(Object.values(vol.toJSON())).toMatchInlineSnapshot(`
-    Array [
-      "[{\\"id\\":\\"entity-1\\",\\"name\\":\\"Entity 1\\",\\"displayName\\":\\"Entity 1\\",\\"createdOn\\":1591831808891,\\"_class\\":\\"Entity\\",\\"_type\\":\\"entity_type_1\\",\\"_key\\":\\"entity-1\\",\\"tag.Production\\":\\"{\\\\\\"object\\\\\\":{\\\\\\"innerProp\\\\\\":\\\\\\"foobar\\\\\\"}}\\"}]",
-      "[{\\"id\\":\\"entity-2\\",\\"name\\":\\"Entity 2\\",\\"displayName\\":\\"Entity 2\\",\\"createdOn\\":1591831808892,\\"_class\\":\\"Entity\\",\\"_type\\":\\"entity_type_2\\",\\"_key\\":\\"entity-2\\"}]",
-      "[{\\"_key\\":\\"entity-1|has|entity-2\\",\\"_type\\":\\"entity_type_1_has_2\\",\\"_class\\":\\"HAS\\",\\"_fromEntityKey\\":\\"entity-1\\",\\"_toEntityKey\\":\\"entity-2\\",\\"displayName\\":\\"HAS\\"}]",
+    [
+      "[{"id":"entity-1","name":"Entity 1","displayName":"Entity 1","createdOn":1591831808891,"_class":"Entity","_type":"entity_type_1","_key":"entity-1","tag.Production":"{\\"object\\":{\\"innerProp\\":\\"foobar\\"}}"}]",
+      "[{"id":"entity-2","name":"Entity 2","displayName":"Entity 2","createdOn":1591831808892,"_class":"Entity","_type":"entity_type_2","_key":"entity-2"}]",
+      "[{"_key":"entity-1|has|entity-2","_type":"entity_type_1_has_2","_class":"HAS","_fromEntityKey":"entity-1","_toEntityKey":"entity-2","displayName":"HAS"}]",
       "id,name,displayName,createdOn,_class,_type,_key,tag.Production
-    entity-1,Entity 1,Entity 1,1591831808891,Entity,entity_type_1,entity-1,\\"{\\"\\"object\\"\\":{\\"\\"innerProp\\"\\":\\"\\"foobar\\"\\"}}\\"",
+    entity-1,Entity 1,Entity 1,1591831808891,Entity,entity_type_1,entity-1,"{""object"":{""innerProp"":""foobar""}}"",
       "id,name,displayName,createdOn,_class,_type,_key
     entity-2,Entity 2,Entity 2,1591831808892,Entity,entity_type_2,entity-2",
       "_key,_type,_class,_fromEntityKey,_toEntityKey,displayName
@@ -122,8 +122,8 @@ test('should only export relationships when specified', async () => {
     ]),
   ).resolves.toEqual(expect.anything());
   expect(Object.values(vol.toJSON())).toMatchInlineSnapshot(`
-    Array [
-      "[{\\"_key\\":\\"entity-1|has|entity-2\\",\\"_type\\":\\"entity_type_1_has_2\\",\\"_class\\":\\"HAS\\",\\"_fromEntityKey\\":\\"entity-1\\",\\"_toEntityKey\\":\\"entity-2\\",\\"displayName\\":\\"HAS\\"}]",
+    [
+      "[{"_key":"entity-1|has|entity-2","_type":"entity_type_1_has_2","_class":"HAS","_fromEntityKey":"entity-1","_toEntityKey":"entity-2","displayName":"HAS"}]",
       "_key,_type,_class,_fromEntityKey,_toEntityKey,displayName
     entity-1|has|entity-2,entity_type_1_has_2,HAS,entity-1,entity-2,HAS",
     ]
@@ -169,15 +169,15 @@ test('should only export entities when specified', async () => {
     ]),
   ).resolves.toEqual(expect.anything());
   expect(Object.values(vol.toJSON())).toMatchInlineSnapshot(`
-          Array [
-            "[{\\"id\\":\\"entity-1\\",\\"name\\":\\"Entity 1\\",\\"displayName\\":\\"Entity 1\\",\\"createdOn\\":1591831808891,\\"_class\\":\\"Entity\\",\\"_type\\":\\"entity_type_1\\",\\"_key\\":\\"entity-1\\",\\"tag.Production\\":\\"{\\\\\\"object\\\\\\":{\\\\\\"innerProp\\\\\\":\\\\\\"foobar\\\\\\"}}\\"}]",
-            "[{\\"id\\":\\"entity-2\\",\\"name\\":\\"Entity 2\\",\\"displayName\\":\\"Entity 2\\",\\"createdOn\\":1591831808892,\\"_class\\":\\"Entity\\",\\"_type\\":\\"entity_type_2\\",\\"_key\\":\\"entity-2\\"}]",
-            "id,name,displayName,createdOn,_class,_type,_key,tag.Production
-          entity-1,Entity 1,Entity 1,1591831808891,Entity,entity_type_1,entity-1,\\"{\\"\\"object\\"\\":{\\"\\"innerProp\\"\\":\\"\\"foobar\\"\\"}}\\"",
-            "id,name,displayName,createdOn,_class,_type,_key
-          entity-2,Entity 2,Entity 2,1591831808892,Entity,entity_type_2,entity-2",
-          ]
-        `);
+    [
+      "[{"id":"entity-1","name":"Entity 1","displayName":"Entity 1","createdOn":1591831808891,"_class":"Entity","_type":"entity_type_1","_key":"entity-1","tag.Production":"{\\"object\\":{\\"innerProp\\":\\"foobar\\"}}"}]",
+      "[{"id":"entity-2","name":"Entity 2","displayName":"Entity 2","createdOn":1591831808892,"_class":"Entity","_type":"entity_type_2","_key":"entity-2"}]",
+      "id,name,displayName,createdOn,_class,_type,_key,tag.Production
+    entity-1,Entity 1,Entity 1,1591831808891,Entity,entity_type_1,entity-1,"{""object"":{""innerProp"":""foobar""}}"",
+      "id,name,displayName,createdOn,_class,_type,_key
+    entity-2,Entity 2,Entity 2,1591831808892,Entity,entity_type_2,entity-2",
+    ]
+  `);
 });
 
 test('should log error when export fails', async () => {
