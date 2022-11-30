84b1790cde3fad99b5ac6297908e49e94f913589
Austin Kelleher <austinlkelleher@gmail.com>
2022-11-30 13:08:30 -0500

    Remove old CLI test that heavily used mocks
    This test was written using Jest mocking functionality. It is difficult
    to understand and the behavior of the underlying Jest libs seems
    to have changed. The test itself isn't adding much value and unit
    tests exist that cover the same cases.

--- packages/integration-sdk-cli/src/__tests__/cli-run-failure.test.ts
+++ packages/integration-sdk-cli/src/__tests__/cli-run-failure.test.ts
@@ -1,23 +1,11 @@
-const mockCreateIntegrationLogger = jest.fn();
-
-jest.doMock('@jupiterone/integration-sdk-runtime', () => ({
-  ...(jest.requireActual('@jupiterone/integration-sdk-runtime') as any),
-  createIntegrationLogger: mockCreateIntegrationLogger,
-}));
-
 import { Polly } from '@pollyjs/core';
 import NodeHttpAdapter from '@pollyjs/adapter-node-http';
 import FSPersister from '@pollyjs/persister-fs';
-
 import { loadProjectStructure } from '@jupiterone/integration-sdk-private-test-utils';
 import { SynchronizationJobStatus } from '@jupiterone/integration-sdk-core';
-
 import { generateSynchronizationJob } from './util/synchronization';
-
 import { createCli } from '../index';
-
 import { setupSynchronizerApi } from './util/synchronization';
-
 import * as log from '../log';
 import { createTestPolly } from './util/recording';
 
@@ -25,10 +13,6 @@ jest.mock('../log');
 
 Polly.register(NodeHttpAdapter);
 Polly.register(FSPersister);
-
-const { createIntegrationLogger } = jest.requireActual(
-  '@jupiterone/integration-sdk-runtime',
-);
 
 let polly: Polly;
 
@@ -38,10 +22,6 @@ beforeEach(() => {
   loadProjectStructure('validationFailure');
 
   polly = createTestPolly('run-cli-failure');
-
-  jest
-    .mocked(mockCreateIntegrationLogger)
-    .mockReturnValue(createIntegrationLogger({ name: 'test' }));
 
   jest.spyOn(process, 'exit').mockImplementation((code: number | undefined) => {
     throw new Error(`Process exited with code ${code}`);
@@ -70,38 +50,4 @@ test('aborts synchronization job if an error occurs', async () => {
     ...job,
     status: SynchronizationJobStatus.ABORTED,
   });
-});
-
-test('does not log errors that have been previously logged', async () => {
-  const job = generateSynchronizationJob();
-
-  setupSynchronizerApi({ polly, job, baseUrl: 'https://api.us.jupiterone.io' });
-
-  const logger = createIntegrationLogger({ name: 'test' });
-
-  const isHandledErrorSpy = jest.spyOn(logger, 'isHandledError');
-  const validationFailureSpy = jest.spyOn(logger, 'validationFailure');
-  const warnSpy = jest.spyOn(logger, 'warn');
-
-  jest.spyOn(logger, 'child').mockReturnValue(logger);
-
-  jest.mocked(mockCreateIntegrationLogger).mockReturnValue(logger);
-
-  await createCli().parseAsync([
-    'node',
-    'j1-integration',
-    'run',
-    '--integrationInstanceId',
-    'test',
-  ]);
-
-  expect(validationFailureSpy).toHaveBeenCalledTimes(1);
-
-  const loggedError = validationFailureSpy.mock.calls[0][0];
-
-  expect(isHandledErrorSpy).toHaveBeenCalledTimes(1);
-  expect(isHandledErrorSpy).toHaveBeenCalledWith(loggedError);
-  expect(isHandledErrorSpy).toHaveReturnedWith(true);
-
-  expect(warnSpy).toHaveBeenCalledTimes(1);
 });
