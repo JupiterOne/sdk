b35148665ea8ee50f3bed40ddf473802506e5a39
Gonzalo Avalos Ribas <gonzaloavalosribas@Gonzalos-MacBook-Pro.local>
2023-07-31 13:38:26 -0300

    format

--- packages/integration-sdk-runtime/src/synchronization/__tests__/batchSize.test.ts
+++ packages/integration-sdk-runtime/src/synchronization/__tests__/batchSize.test.ts
@@ -207,12 +207,12 @@ describe('#createPersisterApiStepGraphObjectDataUploader', () => {
       for (const call of entityCalls) {
         expect(
           Buffer.byteLength(JSON.stringify(call[1].entities)),
-        ).toBeLessThanOrEqual(bytesToBatch *1.01);
+        ).toBeLessThanOrEqual(bytesToBatch * 1.01);
       }
       for (const call of relationshipsCalls) {
         expect(
           Buffer.byteLength(JSON.stringify(call[1].relationships)),
-        ).toBeLessThanOrEqual(bytesToBatch *1.01);
+        ).toBeLessThanOrEqual(bytesToBatch * 1.01);
       }
     }
   });
@@ -256,7 +256,7 @@ describe('#createPersisterApiStepGraphObjectDataUploader', () => {
       );
       expect(
         Buffer.byteLength(JSON.stringify(call[1].entities)),
-      ).toBeLessThanOrEqual(bytesToBatch *1.01);
+      ).toBeLessThanOrEqual(bytesToBatch * 1.01);
     }
     for (const call of relationshipsCalls) {
       relationshipSentToSync = relationshipSentToSync.concat(
@@ -264,7 +264,7 @@ describe('#createPersisterApiStepGraphObjectDataUploader', () => {
       );
       expect(
         Buffer.byteLength(JSON.stringify(call[1].relationships)),
-      ).toBeLessThanOrEqual(bytesToBatch *1.01);
+      ).toBeLessThanOrEqual(bytesToBatch * 1.01);
     }
     //Check that all elements that we wanted to sync were called
     expect(bigObjectData.entities.map((item) => item._key).sort()).toEqual(
@@ -321,12 +321,12 @@ describe('#createPersisterApiStepGraphObjectDataUploader', () => {
       );
       expect(
         Buffer.byteLength(JSON.stringify(call[1].entities)),
-      ).toBeLessThanOrEqual(bytesToBatch *1.01);
+      ).toBeLessThanOrEqual(bytesToBatch * 1.01);
     }
     for (const call of relationshipsCalls) {
       expect(
         Buffer.byteLength(JSON.stringify(call[1].relationships)),
-      ).toBeLessThanOrEqual(bytesToBatch *1.01);
+      ).toBeLessThanOrEqual(bytesToBatch * 1.01);
     }
     expect(bigObjectData.entities.map((item) => item._key).sort()).toEqual(
       sentToSync.sort(),
--- packages/integration-sdk-runtime/src/synchronization/batchBySize.ts
+++ packages/integration-sdk-runtime/src/synchronization/batchBySize.ts
@@ -1,60 +1,80 @@
 import {
-    Entity,
+  Entity,
   IntegrationLogger,
   Relationship,
 } from '@jupiterone/integration-sdk-core';
 import { UploadDataLookup } from '.';
 import { MAX_BATCH_SIZE_IN_BYTES } from './shrinkBatchRawData';
 
-export function batchGraphObjectsBySizeInBytes<T extends UploadDataLookup, K extends keyof T>(
-    graphObjects: T[K][],
-    maximumBatchSizeInBytes: number,
-    logger: IntegrationLogger,
-  ): T[K][][] {
-    const batches: T[K][][] = [];
-  
-    let currentBatch: T[K][] = [];
-    let currentBatchSize: number = 0;
-    for (const graphObject of graphObjects) {
-      const typedGraphObject = graphObject as Entity | Relationship
-      
-      let graphObjectSizeInBytes = getSizeOfObject(graphObject);
-      
-      if (graphObjectSizeInBytes > maximumBatchSizeInBytes) {
-        const rawDataSizeInBytes = typedGraphObject._rawData ? getSizeOfObject(typedGraphObject._rawData) : 0;
+export function batchGraphObjectsBySizeInBytes<
+  T extends UploadDataLookup,
+  K extends keyof T,
+>(
+  graphObjects: T[K][],
+  maximumBatchSizeInBytes: number,
+  logger: IntegrationLogger,
+): T[K][][] {
+  const batches: T[K][][] = [];
 
-        if (graphObjectSizeInBytes-rawDataSizeInBytes > maximumBatchSizeInBytes) {
-          logger.warn({ 
-            _key: typedGraphObject._key, _type: typedGraphObject.type, maximumBatchSizeInBytes, graphObjectSizeInBytes, rawDataSizeInBytes,
-           }, 'Graph object is larger than batch size in bytes; cannot upload graph object.');
-           continue;
-        } else {
-          logger.info({ 
-            _key: typedGraphObject._key, _type: typedGraphObject.type, maximumBatchSizeInBytes, graphObjectSizeInBytes, rawDataSizeInBytes,
-           }, 'Graph object is larger than batch size in bytes; removing raw data.');
-           graphObjectSizeInBytes -= rawDataSizeInBytes;
-           typedGraphObject._rawData = undefined;
-        }
+  let currentBatch: T[K][] = [];
+  let currentBatchSize: number = 0;
+  for (const graphObject of graphObjects) {
+    const typedGraphObject = graphObject as Entity | Relationship;
+
+    let graphObjectSizeInBytes = getSizeOfObject(graphObject);
+
+    if (graphObjectSizeInBytes > maximumBatchSizeInBytes) {
+      const rawDataSizeInBytes = typedGraphObject._rawData
+        ? getSizeOfObject(typedGraphObject._rawData)
+        : 0;
+
+      if (
+        graphObjectSizeInBytes - rawDataSizeInBytes >
+        maximumBatchSizeInBytes
+      ) {
+        logger.warn(
+          {
+            _key: typedGraphObject._key,
+            _type: typedGraphObject.type,
+            maximumBatchSizeInBytes,
+            graphObjectSizeInBytes,
+            rawDataSizeInBytes,
+          },
+          'Graph object is larger than batch size in bytes; cannot upload graph object.',
+        );
+        continue;
+      } else {
+        logger.info(
+          {
+            _key: typedGraphObject._key,
+            _type: typedGraphObject.type,
+            maximumBatchSizeInBytes,
+            graphObjectSizeInBytes,
+            rawDataSizeInBytes,
+          },
+          'Graph object is larger than batch size in bytes; removing raw data.',
+        );
+        graphObjectSizeInBytes -= rawDataSizeInBytes;
+        typedGraphObject._rawData = undefined;
       }
-  
-      if (graphObjectSizeInBytes + currentBatchSize >= maximumBatchSizeInBytes) {
-        batches.push(currentBatch);
-        currentBatch = [];
-        currentBatchSize = 0;
-      }
-      currentBatch.push(graphObject);
-      currentBatchSize += graphObjectSizeInBytes;
     }
-  
-    if (currentBatch.length > 0) {
+
+    if (graphObjectSizeInBytes + currentBatchSize >= maximumBatchSizeInBytes) {
       batches.push(currentBatch);
+      currentBatch = [];
+      currentBatchSize = 0;
     }
-    return batches;
+    currentBatch.push(graphObject);
+    currentBatchSize += graphObjectSizeInBytes;
   }
 
-export function getSizeOfObject(
-  object: any,
-): number {
+  if (currentBatch.length > 0) {
+    batches.push(currentBatch);
+  }
+  return batches;
+}
+
+export function getSizeOfObject(object: any): number {
   try {
     return Buffer.byteLength(JSON.stringify(object), 'utf8');
   } catch (error) {
