8b2e905d40603210851ca5d9a3068d3ce1820eee
Ronald Arias <ronald.arias@contractor.jupiterone.com>
2024-02-07 12:01:28 -0500

    base client with retries and authentication

--- packages/integration-sdk-http-client/src/errors.ts
+++ packages/integration-sdk-http-client/src/errors.ts
@@ -1,17 +1,124 @@
-export interface APIErrorParams {
-  message: string;
+import {
+  IntegrationLogger,
+  IntegrationProviderAPIError,
+  IntegrationProviderAuthenticationError,
+  IntegrationProviderAuthorizationError,
+} from '@jupiterone/integration-sdk-core';
+import { Response } from 'node-fetch';
 
-  code: number;
+export type RateLimitErrorParams = ConstructorParameters<
+  typeof IntegrationProviderAPIError
+>[0] & {
+  retryAfter: number;
+};
+
+interface RequestErrorParams {
+  endpoint: string;
+  response: Response;
+  logger?: IntegrationLogger;
+  logErrorBody: boolean;
 }
 
-export class APIError extends Error {
-  /**
-   * Code associated with the error.
-   */
-  readonly code: number;
+export class RetryableIntegrationProviderApiError extends IntegrationProviderAPIError {
+  retryable = true;
+}
 
-  constructor(config: APIErrorParams) {
-    super(config.message);
-    this.code = config.code;
+export class RateLimitError extends IntegrationProviderAPIError {
+  constructor(options: RateLimitErrorParams) {
+    super(options);
+    this.retryAfter = options.retryAfter;
   }
+  retryAfter: number;
+  retryable = true;
+}
+
+class HTTPResponseError extends Error {
+  response: Response;
+  constructor(response: Response) {
+    super(`HTTP Error Response: ${response.status} ${response.statusText}`);
+    this.response = response;
+  }
+}
+
+export async function retryableRequestError({
+  endpoint,
+  response,
+  logger,
+  logErrorBody,
+}: RequestErrorParams): Promise<RetryableIntegrationProviderApiError> {
+  if (logErrorBody && logger) {
+    let errorBody: any;
+    try {
+      errorBody = await response.json();
+      logger?.error(
+        { errBody: JSON.stringify(errorBody) },
+        'Encountered error from API'
+      );
+    } catch (e) {
+      // pass
+    }
+  }
+
+  if (response.status === 429) {
+    return new RateLimitError({
+      cause: new HTTPResponseError(response),
+      status: response.status,
+      statusText: response.statusText,
+      endpoint,
+      retryAfter: Number(response.headers.get('retry-after')),
+    });
+  }
+
+  return new RetryableIntegrationProviderApiError({
+    cause: new HTTPResponseError(response),
+    endpoint,
+    status: response.status,
+    statusText: response.statusText ?? response.status,
+  });
+}
+
+export async function fatalRequestError({
+  endpoint,
+  response,
+  logger,
+  logErrorBody,
+}: RequestErrorParams): Promise<IntegrationProviderAPIError> {
+  if (logErrorBody && logger) {
+    let errorBody: any;
+    try {
+      errorBody = await response.json();
+      logger?.error(
+        { errBody: JSON.stringify(errorBody) },
+        'Encountered error from API'
+      );
+    } catch (e) {
+      // pass
+    }
+  }
+
+  const apiErrorOptions = {
+    cause: new HTTPResponseError(response),
+    endpoint,
+    status: response.status,
+    statusText: response.statusText ?? response.status,
+  };
+  if (response.status === 401) {
+    return new IntegrationProviderAuthenticationError(apiErrorOptions);
+  } else if (response.status === 403) {
+    return new IntegrationProviderAuthorizationError(apiErrorOptions);
+  } else {
+    return new IntegrationProviderAPIError(apiErrorOptions);
+  }
+}
+
+/**
+ * Function for determining if a request is retryable
+ * based on the returned status.
+ */
+export function isRetryableRequest({ status }: Response): boolean {
+  return (
+    // 5xx error from provider (their fault, might be retryable)
+    // 429 === too many requests, we got rate limited so safe to try again
+    status >= 500 || status === 429
+  );
 }
--- packages/integration-sdk-http-client/src/index.ts
+++ packages/integration-sdk-http-client/src/index.ts
@@ -1,95 +1,210 @@
-import fetch from 'node-fetch';
-import { getUnixTimeNow, sleep, isNonRetryableError } from './util';
+import fetch, { Response } from 'node-fetch';
 import {
-  RateLimitConfig,
-  APIResponse,
-  RateLimitState,
-  APIRequest,
-} from './types';
-import { APIError } from './errors';
+  IntegrationLogger,
+  IntegrationProviderAPIError,
+} from '@jupiterone/integration-sdk-core';
+import { AttemptContext, retry, sleep } from '@lifeomic/attempt';
+import {
+  fatalRequestError,
+  isRetryableRequest,
+  retryableRequestError,
+} from './errors';
 
-export const DEFAULT_RATE_LIMIT_CONFIG: RateLimitConfig = {
-  maxAttempts: 5,
-  reserveLimit: 30,
-  cooldownPeriod: 1000,
-  sleepAdditionalSeconds: 0,
-};
+type OptionalPromise<T> = T | Promise<T>;
+
+interface RequestOptions {
+  method?: 'GET' | 'POST';
+  body?: Record<string, unknown>;
+  headers?: Record<string, string>;
+  authenticate?: boolean;
+}
 
 export type APIResourceIterationCallback<T> = (
-  resources: T[],
+  resources: T[]
 ) => boolean | void | Promise<boolean | void>;
 
-export class APIClient {
-  // TODO (adam-in-ict) make rateLimitConfig configurable and only use the default values when none are provided
-  private rateLimitConfig: RateLimitConfig = DEFAULT_RATE_LIMIT_CONFIG;
-  private rateLimitState: RateLimitState;
+interface RetryOptions {
+  maxAttempts: number;
+  delay: number;
+  factor: number;
+  timeout: number;
+  handleError: (
+    err: any,
+    context: AttemptContext,
+    logger?: IntegrationLogger
+  ) => OptionalPromise<void>;
+}
 
-  async executeAPIRequest(request: APIRequest): Promise<APIResponse> {
-    let attempts = 0;
+interface ClientConfig {
+  baseUrl: string;
+  logger?: IntegrationLogger;
+  retryOptions?: Partial<RetryOptions>;
+  logErrorBody?: boolean;
+}
 
-    do {
-      const response = await fetch(request.url, request);
-
-      if (isNonRetryableError(response.status)) {
-        const err = new APIError({
-          message: `API request error for ${request.url}: ${response.status} ${response.statusText}`,
-          code: response.status,
-        });
-        throw err;
-      }
-
-      // TODO (adam-in-ict) make rate limit error configurable for instances where an API isn't using the default of 429
-      if (response.status === 429) {
-        await this.handleRateLimitError(response.headers);
-      } else {
-        // Successful response, so return
-        return {
-          data: await response.json(),
-          headers: response.headers,
-          status: response.status,
-          statusText: response.statusText,
-        };
-      }
-
-      attempts += 1;
-    } while (attempts < this.rateLimitConfig.maxAttempts);
-
-    throw new APIError({
-      message: `Could not complete request within ${attempts} attempts!`,
-      code: 429,
-    });
+const defaultErrorHandler = async (
+  err: any,
+  context: AttemptContext,
+  logger?: IntegrationLogger
+) => {
+  if (err.code === 'ECONNRESET' || err.message.includes('ECONNRESET')) {
+    return;
   }
 
-  //TODO (adam-in-ict) abstract out which Header values we're grabbing for rate limiting.
-  /**
-   * Get rate limit info, sleep, and then loop to retry
-   *
-   * @param headers
-   */
-  private async handleRateLimitError(headers) {
-    this.rateLimitState = {
-      limitRemaining: Number(headers.get('X-RateLimit-Remaining')),
-      perMinuteLimit: Number(headers.get('X-RateLimit-Limit')),
-      retryAfter:
-        headers.get('X-RateLimit-RetryAfter') &&
-        Number(headers.get('X-RateLimit-RetryAfter')),
-    };
+  if (!err.retryable) {
+    // can't retry this? just abort
+    context.abort();
+    return;
+  }
 
-    const unixTimeNow = getUnixTimeNow();
-    const timeToSleepInSeconds = this.rateLimitState.retryAfter
-      ? Math.max(
-          this.rateLimitState.retryAfter +
-            this.rateLimitConfig.sleepAdditionalSeconds -
-            unixTimeNow,
-          0,
-        )
-      : 0;
-    await sleep(timeToSleepInSeconds * 1000);
-    if (
-      this.rateLimitState.limitRemaining &&
-      this.rateLimitState.limitRemaining <= this.rateLimitConfig.reserveLimit
-    ) {
-      await sleep(this.rateLimitConfig.cooldownPeriod);
+  if (err.status === 429) {
+    const retryAfter = err.retryAfter ? err.retryAfter * 1000 : 5000;
+    logger?.warn(
+      { retryAfter },
+      `Received a rate limit error. Waiting before retrying.`
+    );
+    await sleep(retryAfter);
+  }
+};
+
+const DEFAULT_RETRY_OPTIONS: RetryOptions = {
+  maxAttempts: 3,
+  delay: 30_000,
+  timeout: 180_000,
+  factor: 2, // exponential backoff factor. with 30 sec start and 3 attempts, longest wait is 2 min
+  handleError: defaultErrorHandler,
+};
+
+export abstract class APIClient {
+  protected authenticationHeaders: Record<string, string>;
+  protected logger?: IntegrationLogger;
+  protected retryOptions: RetryOptions;
+  protected logErrorBody: boolean;
+  protected baseUrl: string;
+
+  constructor(config: ClientConfig) {
+    this.baseUrl = config.baseUrl;
+    this.logger = config.logger;
+    this.retryOptions = {
+      ...DEFAULT_RETRY_OPTIONS,
+      ...config.retryOptions,
+    };
+    this.logErrorBody = config.logErrorBody ?? false;
+  }
+
+  protected withBaseUrl(endpoint: string): string {
+    return `${this.baseUrl}${endpoint.startsWith('/') ? '' : '/'}${endpoint}`;
+  }
+
+  /**
+   * Get the authentication headers for the API requests.
+   *
+   * @return {Promise<Record<string, string>>} - The authentication headers
+   *
+   * @example
+   * ```typescript
+   * async getAuthenticationHeaders(): Record<string, string> {
+   *   return {
+   *     Authorization: `Bearer ${this.config.apiKey}`,
+   *   };
+   * }
+   * ```
+   * @example
+   * ```typescript
+   * async getAuthenticationHeaders(): Promise<Record<string, string>> {
+   *   const response = this.request('/token', {
+   *     method: 'POST',
+   *     body: {
+   *       email: this.config.email,
+   *       password: this.config.password,
+   *     },
+   *     authenticate: false, // don't try to authenticate this request, it will go into an infinite loop.
+   *   });
+   *   const data = await response.json();
+   *   return {
+   *     Authorization: `Bearer ${data.token}`,
+   *   };
+   * }
+   */
+  abstract getAuthenticationHeaders(): OptionalPromise<Record<string, string>>;
+
+  /**
+   * Perform a request to the API
+   * @param {string} endpoint - The endpoint to request
+   * @param {RequestOptions} options - The options for the request
+   * @param {string} [options.method=GET] - The HTTP method to use
+   * @param {Record<string, unknown>} [options.body] - The body of the request
+   * @param {boolean} [options.authenticate=true] - Whether to include authentication headers
+   * @return {Promise<Response>} - The response from the API
+   */
+  protected async request(
+    endpoint: string,
+    { method = 'GET', body, headers, authenticate = true }: RequestOptions
+  ): Promise<Response> {
+    if (authenticate && !this.authenticationHeaders) {
+      this.authenticationHeaders = await this.getAuthenticationHeaders();
     }
+    const response = await fetch(this.withBaseUrl(endpoint), {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        ...(authenticate && this.authenticationHeaders),
+        ...headers,
+      },
+      body: body ? JSON.stringify(body) : undefined,
+    });
+    return response;
+  }
+
+  protected async retryRequest(
+    endpoint: string,
+    { method = 'GET', body, authenticate = true }: RequestOptions
+  ): Promise<Response> {
+    return retry(
+      async () => {
+        let response: Response | undefined;
+        try {
+          response = await this.request(endpoint, {
+            method,
+            body,
+            authenticate,
+          });
+        } catch (err) {
+          this.logger?.error(
+            { code: err.code, err, endpoint },
+            'Error sending request'
+          );
+          throw err;
+        }
+
+        if (response.ok) {
+          return response;
+        }
+
+        let error: IntegrationProviderAPIError | undefined;
+        const requestErrorParams = {
+          endpoint,
+          response,
+          logger: this.logger,
+          logErrorBody: this.logErrorBody,
+        };
+        if (isRetryableRequest(response)) {
+          error = await retryableRequestError(requestErrorParams);
+        } else {
+          error = await fatalRequestError(requestErrorParams);
+        }
+        throw error;
+      },
+      {
+        maxAttempts: this.retryOptions.maxAttempts,
+        delay: this.retryOptions.delay,
+        timeout: this.retryOptions.timeout,
+        factor: this.retryOptions.factor,
+        handleError: async (err, context) => {
+          await this.retryOptions.handleError(err, context, this.logger);
+        },
+      }
+    );
   }
 }
