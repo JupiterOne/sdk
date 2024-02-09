e4a0b7adac46253a73edc4b022671bc846511530
Jake Ferrero <jake.ferrero@jupiterone.com>
2024-02-09 18:52:49 -0500

    IP-SPRINT remove the graph project I accidentally added...

--- graph-nick/jest.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('@jupiterone/integration-sdk-dev-tools/config/jest');
--- graph-nick/src/steps/index.ts
+++
@@ -1,7 +0,0 @@
-import { organizationSteps } from './steps/fetch-organizations';
-import { userSteps } from './steps/fetch-users';
-import { firewallSteps } from './steps/fetch-firewalls';
-
-const integrationSteps = [...organizationSteps, ...userSteps, ...firewallSteps];
-
-export { integrationSteps };
