b17f19ef6ec26ce5f6adf9f0110e433fc4a43bc7
Nick-ncsu <nathomp3@alumni.ncsu.edu>
2024-02-09 19:12:20 -0500

    Remove Jake

--- graph-jake-inc/src/steps/index.ts
+++
@@ -1,7 +0,0 @@
-import { organizationSteps } from './fetch-organizations';
-import { userSteps } from './fetch-users';
-import { firewallSteps } from './fetch-firewalls';
-
-const integrationSteps = [...organizationSteps, ...userSteps, ...firewallSteps];
-
-export { integrationSteps };
