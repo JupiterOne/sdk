74048222c16fb125d24db03a0627f42c494263d4
Ronald Arias <ronald.arias@contractor.jupiterone.com>
2024-02-23 15:57:31 -0500

    Implement BaseAPIClient in http-client package (#1024)
    Co-authored-by: Gaston Yelmini <gastonyelmini@gmail.com>
    Co-authored-by: Nick-ncsu <nathomp3@alumni.ncsu.edu>

--- packages/integration-sdk-http-client/src/errors.ts
+++ packages/integration-sdk-http-client/src/errors.ts
@@ -1,17 +1,121 @@
-export interface APIErrorParams {
-  message: string;
+import {
+  IntegrationLogger,
+  IntegrationProviderAPIError,
+  IntegrationProviderAuthenticationError,
+  IntegrationProviderAuthorizationError,
+} from '@jupiterone/integration-sdk-core';
+import { Response } from 'node-fetch';
 
-  code: number;
+type RateLimitErrorParams = ConstructorParameters<
+  typeof IntegrationProviderAPIError
+>[0] & {
+  retryAfter: number;
+};
+
+interface RequestErrorParams {
+  endpoint: string;
+  response: Response;
+  logger: IntegrationLogger;
+  logErrorBody: boolean;
 }
 
-export class APIError extends Error {
-  /**
-   * Code associated with the error.
-   */
-  readonly code: number;
+export class RetryableIntegrationProviderApiError extends IntegrationProviderAPIError {
+  retryable = true;
+}
 
-  constructor(config: APIErrorParams) {
-    super(config.message);
-    this.code = config.code;
+export class RateLimitError extends RetryableIntegrationProviderApiError {
+  constructor(options: RateLimitErrorParams) {
+    super(options);
+    this.retryAfter = options.retryAfter;
   }
+  retryAfter: number;
+}
+
+export class HTTPResponseError extends Error {
+  response: Response;
+  constructor(response: Response) {
+    super(`HTTP Error Response: ${response.status} ${response.statusText}`);
+    this.response = response;
+  }
+}
+
+async function handleLogErrorBody(
+  response: Response,
+  logger: IntegrationLogger,
+  logErrorBody: boolean,
+) {
+  if (!logErrorBody) {
+    return;
+  }
+  let errorBody: any;
+  try {
+    errorBody = await response.json();
+    logger.error(
+      { errBody: JSON.stringify(errorBody) },
+      'Encountered error from API',
+    );
+  } catch (e) {
+    // pass
+  }
+}
+
+export async function retryableRequestError({
+  endpoint,
+  response,
+  logger,
+  logErrorBody,
+}: RequestErrorParams): Promise<RetryableIntegrationProviderApiError> {
+  await handleLogErrorBody(response, logger, logErrorBody);
+
+  if (response.status === 429) {
+    return new RateLimitError({
+      cause: new HTTPResponseError(response),
+      status: response.status,
+      statusText: response.statusText,
+      endpoint,
+      retryAfter: Number(response.headers.get('retry-after')),
+    });
+  }
+
+  return new RetryableIntegrationProviderApiError({
+    cause: new HTTPResponseError(response),
+    endpoint,
+    status: response.status,
+    statusText: response.statusText,
+  });
+}
+
+export async function fatalRequestError({
+  endpoint,
+  response,
+  logger,
+  logErrorBody,
+}: RequestErrorParams): Promise<IntegrationProviderAPIError> {
+  await handleLogErrorBody(response, logger, logErrorBody);
+
+  const apiErrorOptions = {
+    cause: new HTTPResponseError(response),
+    endpoint,
+    status: response.status,
+    statusText: response.statusText,
+  };
+  if (response.status === 401) {
+    return new IntegrationProviderAuthenticationError(apiErrorOptions);
+  } else if (response.status === 403) {
+    return new IntegrationProviderAuthorizationError(apiErrorOptions);
+  } else {
+    return new IntegrationProviderAPIError(apiErrorOptions);
+  }
+}
+
+/**
+ * Function for determining if a request is retryable
+ * based on the returned status.
+ */
+export function isRetryableRequest({ status }: Response): boolean {
+  return (
+    // 5xx error from provider (their fault, might be retryable)
+    // 429 === too many requests, we got rate limited so safe to try again
+    status >= 500 || status === 429
+  );
 }
--- packages/integration-sdk-http-client/src/types.ts
+++ packages/integration-sdk-http-client/src/types.ts
@@ -1,156 +1,54 @@
-export type APIRequest = {
-  url: string;
-  method: string;
-  headers: Object;
-};
+import { IntegrationLogger } from '@jupiterone/integration-sdk-core';
+import { AttemptContext } from '@lifeomic/attempt';
 
-export type APIResponse = {
-  data: any;
-  headers: any;
-  status: number;
-  statusText: string;
-};
+export type OptionalPromise<T> = T | Promise<T>;
 
-// TODO (adam-in-ict), we're not currently paginating anywhere yet.
-type PaginationState = {
-  /**
-   * Fetch limit, will be URL encoded as value of `limit` GET parameter.
-   */
-  limit?: number;
+export interface RequestOptions {
+  method?: 'GET' | 'POST';
+  body?: Record<string, unknown>;
+  headers?: Record<string, string>;
+  authorize?: boolean;
+  bucketTokens?: number;
+}
 
-  /**
-   * Total number of objects reported in API response. The number is affected by
-   * the filter, if any.
-   */
-  total?: number;
-
-  /**
-   * Fetch offset, a number to start from or a cursor, depending on API.
-   */
-  offset?: number | string;
-
-  /**
-   * Offset cursor expiration time, only present for cursor based API.
-   */
-  expiresAt?: number;
-
-  /**
-   * Similar to "offset", a value to start from aka a cursor.
-   */
-  after?: number | string;
-
-  /**
-   * Number of resources returned through pagination to the point of offset.
-   */
-  seen: number;
-
-  /**
-   * Number of pages returned through pagination to the point of offset.
-   */
-  pages: number;
-
-  /**
-   * Pagination has completed according to provided pagination parameters.
-   */
-  finished: boolean;
-};
-
-export type PaginationParams = Partial<PaginationState>;
-
-export type QueryParams = {
-  filter?: string;
-  [name: string]: string | undefined;
-};
-
-export type RateLimitConfig = {
-  /**
-   * The limit remaining value at which the client should slow down. This
-   * prevents the client from consuming all available requests.
-   */
-  reserveLimit: number;
-
-  /**
-   * A recommended period of time in milliseconds to wait between requests when
-   * the `reserveLimit` is reached.
-   *
-   * This can be a value representing the refill rate of a "leaky bucket" or
-   * just a guess about how soon another request can be made. Ideally there will
-   * be enough information in the response headers to calculate a better value.
-   */
-  cooldownPeriod: number;
-
-  /**
-   * Maximum number of times to retry a request that continues to receive 429
-   * responses.
-   *
-   * The client will respect `x-ratelimit-retryafter`, but should it end up in a
-   * battle to get the next allowed request, it will give up after this many
-   * tries.
-   */
+export interface RetryOptions {
   maxAttempts: number;
+  delay: number;
+  factor: number;
+  timeout: number;
+  handleError: (
+    err: any,
+    context: AttemptContext,
+    logger: IntegrationLogger,
+  ) => OptionalPromise<void>;
+}
 
-  /**
-   * Optional number of additional seconds to sleep in the event of a rate limit
-   * event.
-   */
-  sleepAdditionalSeconds: number;
-};
+export interface RateLimitHeaders {
+  limit: string;
+  remaining: string;
+  reset: string;
+}
 
-/**
- * The last seen values from rate limit response headers.
- */
-export type RateLimitState = {
-  /**
-   * Maximum number of requests per minute that can be made by all API clients
-   * in a customer account. Initial value assumes the published default of 100.
-   */
-  perMinuteLimit: number;
+export interface RateLimitThrottlingOptions {
+  threshold: number;
+  rateLimitHeaders?: RateLimitHeaders;
+}
 
-  /**
-   * Number of requests that remain in the account's rate limiting pool. The
-   * total number available is not known.
-   */
-  limitRemaining: number;
+export interface TokenBucketOptions {
+  maximumCapacity: number;
+  refillRate: number;
+}
 
-  /**
-   * The next time when an account's rate limit pool will have at least one
-   * request available.
-   */
-  retryAfter?: number;
-};
+export interface ClientConfig {
+  baseUrl: string;
+  logger: IntegrationLogger;
+  retryOptions?: Partial<RetryOptions>;
+  logErrorBody?: boolean;
+  rateLimitThrottling?: RateLimitThrottlingOptions;
+  tokenBucket?: TokenBucketOptions;
+}
 
-/**
- * Metadata in API responses indicating the pagination state.
- */
-export type PaginationMeta = {
-  limit: number;
-  total: number;
-  offset: number | string;
-  expires_at?: number;
-};
-
-type ResponseMeta = {
-  trace_id: string;
-  pagination?: PaginationMeta;
-};
-
-type ResponseError = {
-  code: number;
-  message: string;
-};
-
-export type ResourcesResponse<T> = {
-  meta: ResponseMeta;
-  errors: ResponseError[];
-  resources: T[];
-};
-
-export type APIClientRateLimitConfig = {
-  limitHeader: string;
-  limitRemainingHeader: string;
-  retryAfterHeader: string;
-};
-
-export type APIClientConfiguration = {
-  rateLimitConfig: APIClientRateLimitConfig;
-};
+export interface IterateCallbackResult {
+  nextUrl?: string;
+  nextRequestOptions?: RequestOptions;
+}
