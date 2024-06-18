6784928a1d230214530fec14ca242a988e37617e
Ryan Willis <ryan.willis@jupiterone.com>
2024-06-18 14:31:26 -0700

    oof

--- packages/integration-sdk-core/src/data/__tests__/createIntegrationHelpers.test.ts
+++ packages/integration-sdk-core/src/data/__tests__/createIntegrationHelpers.test.ts
@@ -170,6 +170,119 @@ describe('createIntegrationHelpers', () => {
     });
   });
 
+  test.only('yo', () => {
+    const [YO, createYo] = createEntityMetadata({
+      resourceName: 'Yo',
+      _class: ['Device', 'Host'],
+      _type: 'yo',
+      description: 'Yo description',
+      schema: Type.Object({
+        id: Type.String(),
+        name: Type.String(),
+        someNewProperty: Type.String(),
+        thisOneIsNotRequired: Type.Optional(Type.String()),
+      }),
+    });
+
+    createYo({
+      id: '1',
+      _key: 'id:123',
+      // hostname: 'hostname',
+      serial: '123456',
+      name: 'entity',
+      // someNewProperty: 'someNewProperty',
+      make: 'here',
+      model: 'there',
+      deviceId: 'yolo',
+      displayName: 'hey',
+      category: 'yolo',
+      lastSeenOn: 0,
+      // _class: ['Device', 'Host'],
+      // _type: 'yo'
+    });
+
+    // const validator = new EntityValidator({
+    //   schemas: Object.values(entitySchemas),
+    // });
+
+    // validator.addSchemas(YO.schema);
+
+    // const { errors } = validator.validateEntity(
+    //   createYo({
+    //     id: '1',
+    //     _key: 'id:123',
+    //     serial: '123456',
+    //     name: 'entity',
+    //   }),
+    // );
+
+    // expect(errors).toEqual([
+    //   {
+    //     dataPath: '',
+    //     keyword: 'required',
+    //     message: "should have required property 'someNewProperty'",
+    //     params: { missingProperty: 'someNewProperty' },
+    //     schemaPath: '#/required',
+    //   },
+    // ]);
+
+    expect(YO).toEqual({
+      resourceName: 'Yo',
+      _class: ['Device', 'Host'],
+      _type: 'yo',
+      schema: {
+        $id: '#yo',
+        description: 'Yo description',
+        allOf: [
+          { $ref: '#Device' },
+          { $ref: '#Host' },
+          {
+            allOf: [
+              {
+                properties: {
+                  _class: {
+                    type: 'array',
+                    items: [
+                      {
+                        const: 'Device',
+                        type: 'string',
+                      },
+                      {
+                        const: 'Host',
+                        type: 'string',
+                      },
+                    ],
+                    additionalItems: false,
+                    maxItems: 2,
+                    minItems: 2,
+                  },
+                  _type: { const: 'yo', type: 'string' },
+                },
+                type: 'object',
+                required: ['_class', '_type'],
+              },
+              {
+                properties: {
+                  id: { type: 'string' },
+                  name: { type: 'string' },
+                  someNewProperty: {
+                    type: 'string',
+                  },
+                  thisOneIsNotRequired: {
+                    type: 'string',
+                  },
+                },
+                required: ['id', 'name', 'someNewProperty'],
+                type: 'object',
+              },
+            ],
+            type: 'object',
+          },
+        ],
+      },
+    });
+  });
+
   test('createIntegrationEntity schema should validate createEntity return', () => {
     const validator = new EntityValidator({
       schemas: Object.values(entitySchemas),
